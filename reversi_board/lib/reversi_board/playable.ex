1e5a95a9465893a3f3155ab668356eeee8b7d128
m-ueno <m-ueno@users.noreply.github.com>
2017-10-12 00:56:10 +0900

    Add Playable protocol

--- reversi_board/lib/reversi_board/playable.ex
+++ reversi_board/lib/reversi_board/playable.ex
@@ -1,3 +1,6 @@
-# defprotocol ReversiBoard.Playable do
-#   def make_step
-# end
+defprotocol ReversiBoard.Playable do
+  alias ReversiBoard.Board
+
+  @doc "make_step returns %Step{x, y, stone} or :skip"
+  def make_step(player, board, stone)
+end
