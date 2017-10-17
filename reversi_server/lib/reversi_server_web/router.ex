b6d0b420e9912df9bd127fad2ffaf420aad3d742
m-ueno <m-ueno@users.noreply.github.com>
2017-10-18 00:46:19 +0900

    Generate player_id in server

--- reversi_server/lib/reversi_server_web/router.ex
+++ reversi_server/lib/reversi_server_web/router.ex
@@ -7,6 +7,7 @@ defmodule ReversiServerWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :put_user_token
   end
 
   pipeline :api do
@@ -18,6 +19,11 @@ defmodule ReversiServerWeb.Router do
 
     get "/", PageController, :index
     get "/game", GameController, :index
+  end
+
+  defp put_user_token(conn, _) do
+    generated_player_id = ReversiServer.Application.generate_player_id
+    assign(conn, :player_id, generated_player_id)
   end
 
   # Other scopes may use custom stacks.
