ca9087825a7a2f3ebe383143e46cf76b04e0087a
m-ueno <m-ueno@users.noreply.github.com>
2017-10-19 02:11:02 +0900

    Add client boilerplate

---
+++ reversi_server/assets/js/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Code from './Code.jsx';
+
+class App extends Component {
+  render() {
+    return (<div>
+      <h1>Hello React :)</h1>
+      <Code />
+      <Code />
+    </div>);
+  }
+}
+export default App;
