fdcfb4e173497014a62480456a04e11427954ac7
m-ueno <m-ueno@users.noreply.github.com>
2017-10-22 18:51:17 +0900

    JoinButton receive gameID in props

--- reversi_server/assets/js/src/containers/JoinButton.jsx
+++ reversi_server/assets/js/src/containers/JoinButton.jsx
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { joinChannel } from '../redux/actions';
 import PureButton from '../components/Button.jsx';
@@ -7,10 +8,14 @@ const mapStateToProps = (state) => ({
   gameID: state.gameID,
 })
 
-const mapDispatchToProps = (dispatch, _ownProps) => ({
-  handleClick: () => dispatch(joinChannel({gameID: 0})),
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  handleClick: () => dispatch(joinChannel({gameID: ownProps.gameID})),
 });
 
 const JoinButton = connect(mapStateToProps, mapDispatchToProps)(PureButton);
+JoinButton.propTypes = {
+  gameID: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+}
 
 export default JoinButton;
