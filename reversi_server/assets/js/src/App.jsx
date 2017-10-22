e1f30bbaa58e3838c341ed619650e60b194061b3
m-ueno <m-ueno@users.noreply.github.com>
2017-10-22 16:31:51 +0900

    App.jsx: Add admin panel and game join buttons
    App.jsx
    
    App.jsx: Set joinbutton gameID
    
    App.jsx summary admin

--- reversi_server/assets/js/src/App.jsx
+++ reversi_server/assets/js/src/App.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import Board from './containers/Board.jsx';
 import JoinButton from './containers/JoinButton.jsx';
+import AdminButton from './containers/AdminButton.jsx';
+import BoardGrid from './containers/BoardGrid.jsx';
 
 class App extends Component {
   render() {
     return (<div>
-      <JoinButton />
+      <JoinButton value="Join 'room1'" gameID="1" />
+      <JoinButton value="Join 'room2'" gameID="2" />
+      <JoinButton value="Join 'room3'" gameID="3" />
       <Board />
+      <details>
+        <summary>Admin zone</summary>
+        <AdminButton />
+        <BoardGrid />
+      </details>
     </div>);
   }
 }
