1577b34002f5c3d317da6e02fe2fe74a36c0cbc4
m-ueno <m-ueno@users.noreply.github.com>
2017-10-22 16:16:36 +0900

    redux: reducer

--- reversi_server/assets/js/src/redux/reducers.js
+++ reversi_server/assets/js/src/redux/reducers.js
@@ -6,6 +6,7 @@ const initialState = {
   myStone: C.STONE_WHITE,
   cells: Array.from(Array(64)).map(_ => C.STONE_SPACE),
   channel: undefined, // Socket.channel
+  games: [],
 };
 
 export default function reversiClient(state = initialState, action) {
@@ -18,6 +19,8 @@ export default function reversiClient(state = initialState, action) {
       return { ...state, gameID, turn, cells, channel };
     case C.BOARD_UPDATED:
       return ({ ...state, cells: action.cells });
+    case C.ALL_GAMES_UPDATED:
+      return ({ ...state, games: action.games });
     default:
       return state;
   }
