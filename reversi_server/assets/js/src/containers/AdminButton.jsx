3c0c1d58c200a30b7b92082a1708339451a37581
m-ueno <m-ueno@users.noreply.github.com>
2017-10-22 18:34:49 +0900

    Add Admin Button

---
+++ reversi_server/assets/js/src/containers/AdminButton.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+
+import { joinAdminChannel } from '../redux/actions';
+import Button from '../components/Button.jsx';
+
+const mapDispatchToProps = (dispatch, _ownProps) => ({
+  handleClick: () => dispatch(joinAdminChannel()),
+  value: "Join Admin channel",
+});
+
+const AdminButton = connect(null, mapDispatchToProps)(Button);
+
+export default AdminButton;
