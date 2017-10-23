58a4f683ce9019184afc9713954506d22ac177c6
m-ueno <m-ueno@users.noreply.github.com>
2017-10-23 21:10:50 +0900

    Remove unused

--- reversi_server/assets/js/src/Code.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Code extends Component {
-  render() {
-    return(<div>
-      <pre><code>Hello world :)</code></pre>
-    </div>);
-  }
-}
