a980e21f7574af1dd8508e9f9b3584aac404a72d
m-ueno <m-ueno@users.noreply.github.com>
2017-10-26 00:07:13 +0900

    Remove redux-ws

--- reversi_server/assets/js/app.js
+++ reversi_server/assets/js/app.js
@@ -4,16 +4,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, applyMiddleware } from 'redux';
-import createSocketMiddleware from 'redux-ws';
 import thunk from 'redux-thunk';
 
 import reducer from './src/redux/reducers';
 import Root from './src/containers/Root.jsx';
-import socket from './src/socket';
+import socketMiddleware from './src/redux/middlewares/socketMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const socketMiddleware = createSocketMiddleware(socket);
 const store = createStore(
   reducer,
   composeEnhancers(applyMiddleware(socketMiddleware, thunk))
--- reversi_server/assets/js/src/socket.js
+++
@@ -1,6 +0,0 @@
-import { Socket } from 'phoenix';
-
-const socket = new Socket('/socket', { params: { token: window.userToken, player_id: window.player_id } });
-socket.connect();
-
-export default socket;
